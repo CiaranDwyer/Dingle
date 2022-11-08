0ed06c5fd344ba34bf072999c75618684f576142
Ciaran Dwyer <ciarandwyer2005@gmail.com>
2022-11-08 12:17:09 +0000

    Commit to my Local Repo

---
+++ src/com/codewithciaran/Test1.java
@@ -0,0 +1,15 @@
+package com.codewithciaran;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        String test = "Automation";
+
+        if (test == "Automation") {
+            System.out.println("Automation is in progress");
+        } else {
+            System.out.println("Do you want to start automation?");
+        }
+    }
+}
